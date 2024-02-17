b6708067b36e5386d4deca6ecf9fc3ab44b03f39
RamyaRP <152672121+RamyMe@users.noreply.github.com>
2024-02-17 18:07:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+var timer=10;
+setTimeout(()=>{
+    document.getElementById('countdown').innerText = timer;
+    timer--;
+    setTimeout(()=>{
+        document.getElementById('countdown').innerText = timer;
+        timer--;
+        setTimeout(()=>{
+            document.getElementById('countdown').innerText = timer;
+            timer--;
+            setTimeout(()=>{
+                document.getElementById('countdown').innerText = timer;
+                timer--;
+                setTimeout(()=>{
+                    document.getElementById('countdown').innerText = timer;
+                    timer--;
+                    setTimeout(()=>{
+                        document.getElementById('countdown').innerText = timer;
+                        timer--;
+                        setTimeout(()=>{
+                            document.getElementById('countdown').innerText = timer;
+                            timer--;
+                            setTimeout(()=>{
+                                document.getElementById('countdown').innerText = timer;
+                                timer--;
+                                setTimeout(()=>{
+                                    document.getElementById('countdown').innerText = timer;
+                                    timer--;
+                                    setTimeout(()=>{
+                                        document.getElementById('countdown').innerText = timer;
+                                        timer--;
+                                        setTimeout(()=>{
+                                            document.getElementById('countdown').innerText = "Happy Independence Day!!!";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+
+
+
+
